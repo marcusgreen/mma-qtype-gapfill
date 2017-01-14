b16257463e7c069e2b79e3459b783301cb1fe60c
Marcus Green <marcusavgreen@gmail.com>
2017-01-14 10:36:49 +0000

    change cursor to pointer when over droptargets

--- directive.js
+++ directive.js
@@ -13,9 +13,8 @@
 // limitations under the License.
 
 angular.module('mm.addons.qtype_gapfill')
-
         /**
-         * Directive to render a Word Select question.
+         * Directive to render a Gapfill question.
          *
          * @module mm.addons.qtype_gapfill
          * @ngdoc directive
@@ -30,7 +29,6 @@ angular.module('mm.addons.qtype_gapfill')
                 link: function (scope) {
                     var question = scope.question,
                             questionEl,
-                            introduction,
                             content;
 
                     if (!question) {
@@ -43,7 +41,7 @@ angular.module('mm.addons.qtype_gapfill')
                     // Get question content.
                     content = questionEl[0].querySelector('.qtext');
                     if (!content) {
-                        log.warn('Aborting because of an error parsing question.', question.name);
+                        $log.warn('Aborting because of an error parsing question.', question.name);
                         return $mmQuestionHelper.showDirectiveError(scope);
                     }
 
@@ -64,26 +62,49 @@ angular.module('mm.addons.qtype_gapfill')
                         parts = selector.split(":");
                         selector = parts[0] + "\\:" + parts[1];
                         selection = document.querySelector(selector);
-                        if (selection == null) {
+                        if ((selection === null) || (angular.element(selection).hasClass('readonly'))) {
                             /* selection will be null after marking/readonly */
                             return;
                         }
                         selection = angular.element(selection);
                         if (selection.hasClass('draggable')) {
                             if (selection.hasClass('picked')) {
+                                /*if picked it set this must be a second
+                                 * click so set it back to show as unpicked
+                                 */
                                 selection.removeClass('picked');
                                 selection.attr('title', '');
+                                document.querySelector('.qtext').style.cursor = "auto";
+                                last_item_clicked = "";
+
                             } else {
+                                /*set border to solid on all words */
+                                draggables = document.querySelectorAll('.draggable');
+                                draggables.forEach(function (draggable) {
+                                    angular.element(draggable).css('border', 'solid 1px');
+                                });
                                 selection.addClass('picked');
                                 selection.attr('title', 'picked');
+                                selection.css('border', 'solid 0px');
+                                droptargets = document.querySelectorAll('.droptarget');
+                                droptargets.forEach(function (droptarget) {
+                                    angular.element(droptarget).css('cursor', 'pointer');
+                                });
                                 last_item_clicked = event.target.innerText;
                             }
                         }
 
                         if (selection.hasClass('droptarget')) {
-                            selection[0].value = last_item_clicked;
+                            if (last_item_clicked !== "") {
+                                selection[0].value = last_item_clicked;
+                            }
+                            /*set border to solid on all words */
+                            draggables = document.querySelectorAll('.draggable');
+                            draggables.forEach(function (draggable) {
+                                angular.element(draggable).css('border', 'solid 1px');
+                            });
                         }
-                        
+
                     };
                 }
             };
